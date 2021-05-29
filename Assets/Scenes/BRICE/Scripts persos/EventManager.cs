3493e65c6473fb178ba147fd5a10a07eb8f0b3e8
ExLZyor <brice.vachez@insa-lyon.fr>
2021-05-29 18:32:18 +0200

    Added transtion to second part

--- Assets/Scenes/BRICE/Scripts persos/EventManager.cs
+++ Assets/Scenes/BRICE/Scripts persos/EventManager.cs
@@ -20,4 +20,14 @@ public class EventManager : MonoBehaviour
     {
         Debug.Log("Start clicked !");
     }
+
+    public void btn_chooseConfigClicked()
+    {
+        Debug.Log("Switching to config selector...");
+        GameObject camera = GameObject.Find("Main Camera");
+
+        camera.GetComponent<CameraMovement>().SelectTransition(
+            CameraMovement.Transition.MAINMENU_CONFIGSELECTOR
+            );
+    }
 }
